61f01084eca8d7373e8042b14d1bacc9cde39fcd
Ciera Twork <CieraMarie1129@gmail.com>
2019-09-11 09:23:15 -0400

    Adds conditional code to banner so we can import it.

--- banner.py
+++ banner.py
@@ -1,18 +1,17 @@
 
-print("================")
-print("     BANNER     ")
-print("    By Ciera    ")
-print("================")
-print("")
-
 def banner(subject, author):
     subject_length = len(subject)
     by_line = f"By {author}"
-    banner_length = subject_length = 4
-    print("================")
-    print(f"     {subject}     ")
-    print(f"    {by_line}    ")
-    print("================")
+    by_line_length = len(by_line)
+    banner_length = max(subject_length, by_line_length) + 4
+    print(f"{'=' * banner_length}")
+    print(f"{subject:^{banner_length}}")
+    print(f"{by_line:^{banner_length}}")
+    print(f"{'=' * banner_length}")
     print("")
 
-banner("HELLO WORLD", "Ciera")
+if __name__ == "__main__":
+    banner("BANNER", "Ciera Twork")
+    subject = input("What's the subject?")
+    author = input("Who's the author?")
+    banner(subject, author)
